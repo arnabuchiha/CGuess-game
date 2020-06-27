b6cf96bc65a4e9b7626f52442fce40401f68059b
arnabuchiha <arnabkeya04@gmail.com>
2020-06-27 23:29:49 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div class="container">
+    <div class="container App" >
   <div class="row">
     <div class="col-sm">
     </div>
     <div class="col-sm">
-      One of three columns
+      <h1 class="display-1">CGuess</h1>
+      <div class="row">
+        <div class="col ma4" style={{backgroundColor:"#FFFF"}}>
+          sdad
+        </div>
+        <div class="col ma4" style={{backgroundColor:"#FFFF"}}>
+          sdad
+        </div>
+      </div>
     </div>
     <div class="col-sm">
     </div>
