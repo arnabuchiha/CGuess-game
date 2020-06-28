cd03727d1b472122ef7f4a5758ec522402e33c74
arnabuchiha <arnabkeya04@gmail.com>
2020-06-28 19:56:18 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div class="container App" >
-  <div class="row">
-    <div class="col-sm">
-      <h1 class="display-1">CGuess</h1>
-      <div class="row">
-        <div class="col mr-4 rounded bg_yellow">
+    <div className="container App" >
+  <div className="row">
+    <div className="col-sm">
+      <h1 className="display-1">CGuess</h1>
+      <div className="row h-100">
+        <div className="col mr-4 rounded bg_yellow">
         <form>
-          <div class="form-group mt-4">
-            <input type="text" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Username"/>
+          <div className="form-group mt-4 d-flex justify-content-center">
+            <input type="text" className="form-control w-50" id="username" aria-describedby="usernameHelp" placeholder="Username"/>
           </div>
-          <button type="submit" class="btn shadow-sm btn-lg text-dark mb-4 bg_green">Play</button>
+          <button type="submit" className="btn shadow-sm btn-lg text-dark mb-4 bg_green">Play</button>
           </form>
         </div>
-        <div class="col ml-4 rounded bg_yellow" >
+        <div className="col ml-4 rounded bg_yellow" >
           How to play?
         </div>
       </div>
