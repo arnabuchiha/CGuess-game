20da9b939fe507086abff0df47a9921de448d227
unknown <ananychitranshi0701@gmail.com>
2020-07-05 20:55:41 +0530

    Update how to play alert

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -22,6 +22,13 @@ export class Login extends React.Component {
     if(this.state.play){
       window.location.href="/game";
     }
+
+    function handlePlay()
+    {
+      var instructions='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+      alert(instructions);
+    }
     // return(
     //   <Redirect to="/game"/>
     // )
@@ -48,7 +55,7 @@ export class Login extends React.Component {
           </button>
           
         </div>
-        <button onClick={console.log("Hello")} type="button" className="instruct" style={{backgroundColor:"#BED9A6",fontFamily:"CustomFont",color:"black"}}>
+        <button onClick={handlePlay} type="button" className="instruct" style={{backgroundColor:"#BED9A6",fontFamily:"CustomFont",color:"black"}}>
             How to play?
           </button>
       </div>
