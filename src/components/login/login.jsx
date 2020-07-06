800f3f276a767e2bd56c768b6ea0d4822264ca89
arnabuchiha <arnabkeya04@gmail.com>
2020-07-06 13:19:16 +0530

    Modal Working

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -48,8 +48,7 @@ export class Login extends React.Component {
           
         </div>
         <Modal show={this.state.show} handleClose={this.hideModal}>
-          <p>Modal</p>
-          <p>Data</p>
+          <p>When its your turn to draw, you will have to choose a word from three options and visualize that word in 80 seconds, alternatively when somebody else is drawing you have to type your guess into the chat to gain points, be quick, the earlier you guess a word the more points you get!</p>
         </Modal>
         <button onClick={this.showModal} id="instruct" type="button" className="instruct" style={{backgroundColor:"#BED9A6",fontFamily:"CustomFont",color:"black"}}>
             How to play?
