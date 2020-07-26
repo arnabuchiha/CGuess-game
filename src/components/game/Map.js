c58ab7c78119bd90f84ca621b1dfb3d7e93ae5f6
arnabuchiha <arnabkeya04@gmail.com>
2020-07-26 12:53:29 +0530

    idea changed

--- src/components/game/Map.js
+++ src/components/game/Map.js
@@ -18,11 +18,14 @@ class Map extends Component{
         googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`
         // googleMapScript.src=`https://maps.googleapis.com/maps/api/staticmap?key=${GOOGLE_MAP_API_KEY}&center=48.26814285239378,-113.94149294602335&zoom=6&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xebe3cd&style=element:labels.text.fill%7Ccolor:0x523735&style=element:labels.text.stroke%7Ccolor:0xf5f1e6&style=feature:administrative%7Celement:geometry%7Cvisibility:off&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0xc9b2a6&style=feature:administrative.land_parcel%7Celement:geometry.stroke%7Ccolor:0xdcd2be&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xae9e90&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x93817c&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0xa5b076&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x447530&style=feature:road%7Cvisibility:off&style=feature:road%7Celement:geometry%7Ccolor:0xf5f1e6&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road.arterial%7Celement:geometry%7Ccolor:0xfdfcf8&style=feature:road.highway%7Celement:geometry%7Ccolor:0xf8c967&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0xe9bc62&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0xe98d58&style=feature:road.highway.controlled_access%7Celement:geometry.stroke%7Ccolor:0xdb8555&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x806b63&style=feature:transit%7Cvisibility:off&style=feature:transit.line%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:transit.line%7Celement:labels.text.fill%7Ccolor:0x8f7d77&style=feature:transit.line%7Celement:labels.text.stroke%7Ccolor:0xebe3cd&style=feature:transit.station%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:water%7Celement:geometry.fill%7Ccolor:0xb9d3c2&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x92998d&size=480x360`
         window.document.body.appendChild(googleMapScript)
-    
+
         googleMapScript.addEventListener('load',()=> {
             
             // this.marker = this.createMarker()
             this.googleMap = this.createGoogleMap()
+            this.dirService=new window.google.maps.DirectionsService();
+            this.dirRenderer=new window.google.maps.DirectionsRenderer({suppressMarkers:true});
+            this.dirRenderer.setMap(this.googleMap)
             // this.googleMap.set('styled_map', this.styledMapType);
             // this.googleMap.setMapTypeId('styled_map');
             this.geocoder = new window.google.maps.Geocoder;
@@ -30,7 +33,7 @@ class Map extends Component{
                 // console.log(mapsMouseEvent.latLng.lng())
                 var position = {lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng()};
                 console.log(position)
-                var marker=new window.google.maps.Marker({
+                this.userMarker=new window.google.maps.Marker({
                     position: position,
                     map: this.googleMap,
                 })
@@ -40,8 +43,10 @@ class Map extends Component{
                         var infowindow=new window.google.maps.InfoWindow({
                             content:contentString
                         });
-                        infowindow.open(this.googleMap, marker);
-                        console.log(results);
+                        infowindow.open(this.googleMap, this.userMarker);
+                        this.zoomMap();
+                        this.generateRandomCoordinate();
+                        this.highlightTDMap();
                     }
                 })
             });
@@ -49,6 +54,46 @@ class Map extends Component{
 
         })
         
+    }
+    zoomMap=()=> {
+        this.googleMap.setZoom(15);
+        this.googleMap.setCenter(this.userMarker.position);
+    }
+    generateRandomCoordinate=()=> {
+        var location = new window.google.maps.LatLng(
+            this.userMarker.getPosition().lat() + (Math.random() / 100 * ((Math.round(Math.random()) % 2) ? 1 : -1)),
+            this.userMarker.getPosition().lng() + (Math.random() / 100 * ((Math.round(Math.random()) % 2) ? 1 : -1))
+        );
+        this.generatedMarker = new window.google.maps.Marker({
+            position: location,
+            map: this.googleMap
+        });
+    }
+    highlightTDMap=()=> {
+        // highlight a street
+        var request = {
+            origin: this.userMarker.getPosition().lat() + "," + this.userMarker.getPosition().lng(),
+            destination: this.generatedMarker.getPosition().lat() + "," + this.generatedMarker.getPosition().lng(),
+            travelMode: window.google.maps.TravelMode.DRIVING
+        };
+        this.userMarker.setMap(null);
+        this.generatedMarker.setMap(null);
+        this.dirService.route(request, (result, status)=> {
+            if (status == window.google.maps.DirectionsStatus.OK) {
+                this.TDMapRoute = result.routes[0];
+                this.dirRenderer.setDirections(result);
+                new window.google.maps.Marker({
+                    position: this.TDMapRoute.overview_path[0],
+                    map: this.googleMap,
+                    icon: "http://maps.google.com/mapfiles/kml/pal2/icon13.png"
+                });
+                new window.google.maps.Marker({
+                    position: this.TDMapRoute.overview_path[this.TDMapRoute.overview_path.length - 1],
+                    map: this.googleMap,
+                    icon: "http://maps.google.com/mapfiles/kml/pal2/icon10.png"
+                });
+            }
+        });
     }
     createGoogleMap = () =>
     new window.google.maps.Map(this.googleMapRef.current, {
@@ -232,7 +277,6 @@ class Map extends Component{
         }
     ]
     })
-  
     createMarker = (position) =>
       new window.google.maps.Marker({
         position: position,
